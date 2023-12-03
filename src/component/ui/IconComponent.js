1210d0c8da743131addbf5924ad12904698c8a4b
Phong Huynh <phonghqm@gmail.com>
2023-12-03 16:27:32 +0700

    update file name

--- src/component/ui/IconComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const IconComponent = ({ icon, alt }) => {
-  return (
-    <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={alt} />
-  );
-};
-
-export default IconComponent;
